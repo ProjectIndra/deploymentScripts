cf158a2a5ef7f2ceaf06db485b6a982176e45647
Avinash reddy.c <avichinnu22@gmail.com>
2025-05-03 23:29:51 +0530

    Update hdfs.sh

--- hdfs.sh
+++ hdfs.sh
@@ -22,5 +22,85 @@ wget https://dlcdn.apache.org/hadoop/common/hadoop-3.4.1/hadoop-3.4.1.tar.gz
 tar -xvzf hadoop-3.4.1.tar.gz
 rm -rf hadoop-3.4.1.tar.gz
 
+# Configure hdfs-site.xml
+cat <<EOL > $HADOOP_CONF_DIR/hdfs-site.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+<!--
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License. See accompanying LICENSE file.
+-->
+
+<!-- Put site-specific property overrides in this file. -->
+
+<configuration>
+ <property>
+   <name>dfs.namenode.name.dir</name>
+   <value>/hdfs</value>
+ </property>
+ <property>
+   <name>dfs.replication</name>
+   <value>1</value>
+ </property>
+ <property>
+   <name>dfs.webhdfs.enabled</name>
+   <value>true</value>
+ </property>
+</configuration>
+EOL
+
+# Configure core-site.xml
+cat <<EOL > $HADOOP_CONF_DIR/core-site.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+<!--
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License. See accompanying LICENSE file.
+-->
+
+<!-- Put site-specific property overrides in this file. -->
+
+<configuration> 
+ <property> 
+   <name>fs.defaultFS</name> 
+   <value>hdfs://0.0.0.0:9000</value>  
+ </property> 
+ <property>
+   <name>hadoop.proxyuser.dataflair.groups</name>
+   <value>*</value>
+ </property>
+ <property>
+   <name>hadoop.proxyuser.dataflair.hosts</name>
+   <value>*</value>
+ </property>
+ <property>
+   <name>hadoop.proxyuser.server.hosts</name>
+   <value>*</value>
+ </property>
+ <property>
+   <name>hadoop.proxyuser.server.groups</name>
+   <value>*</value>
+ </property>
+</configuration>
+EOL
+
 # Output success message
-echo "Hadoop 3.4.1 has been installed and environment variables set."
+echo "Hadoop 3.4.1 has been installed and configuration files have been set."
