7bf2f9a3fddd8ddf79b58ee885c615ae78b2b035
Avinash reddy.c <avichinnu22@gmail.com>
2025-05-04 21:53:11 +0530

    Update wireguard.sh

--- wireguard.sh
+++ wireguard.sh
@@ -1,3 +1,18 @@
+# -If you are using GCP, add firewall in networking in gcp and set it for the vm  instance. UPD with port 51820
+
+# -Windows network config in powershell adminNew-NetFirewallRule -DisplayName "Allow WireGuard" -Direction Inbound -InterfaceAlias "<windows client interface>" -Action Allow
+
+# -netsh advfirewall firewall add rule name="Allow ICMPv4" protocol=icmpv4 dir=in action=allow
+# -To check packetssudo tcpdump -i wg0
+# -Also in relay server add this /etc/sysctl.conf and add:
+# -net.ipv4.ip_forward=1Also run this command
+# -sudo ip route add 10.0.0.0/24 dev wg0sudo iptables -A FORWARD -i wg0 -o wg0 -j ACCEPTsudo ----iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  ( automatically addin in network server )
+
+# Also makesure you put 
+# -sudo ip link set dev wg0 mtu 1200  
+# If ssh fails at the 
+# debug1: expecting SSH2_MSG_KEX_ECDH_REPLY in verbose ssh
+
 #!/bin/bash
 
 set -e
